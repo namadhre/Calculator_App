363c6c94b68ecf384d1f7a857ea0bdba0fc45a85
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-20 21:36:41 +0530

    Created Button.js Added script to the buttons that converts jsx to html tag

---
+++ src/components/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Button.css";
+
+const isOperator = (value) => {
+    return !isNaN(value) || value === "." || value === "-/+" || value === "%";
+};
+
+export const Button = (props) => {
+    return <div
+        className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}
+        onClick={() => props.handleClick(props.children)}>
+        {props.children}
+    </div>
+}
