dadf423029afe8154011217d7605a24b38f618d1
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-21 09:33:08 +0530

    Modified: Button.js. Made code more readable.

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,10 +5,12 @@ const isOperator = (value) => {
     return !isNaN(value) || value === "." || value === "-/+" || value === "%";
 };
 
-export const Button = (props) => {
+const Button = (props) => {
     return <div
         className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}
         onClick={() => props.handleClick(props.children)}>
         {props.children}
     </div>
 }
+
+export default Button;
