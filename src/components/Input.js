2ffdb5dac70fffc9f6d90397ff8beb180e10ab6b
mahendrachinthamgari <mahendra.chinthamgari.22.1@mountblue.tech>
2023-01-21 09:34:42 +0530

    Modified Input.js Made code more readable.

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,8 @@
 import React from "react";
 import "./Input.css";
 
-export const Input = (props) => {
+const Input = (props) => {
     return <div className="input">{props.input}</div>;
 }
+
+export default Input;
